238cadeb59589efd533bfc894615b2662442335d
PatriotRossii <patriotrossii2019@mail.ru>
2022-10-28 21:46:27 +0300

    Use python-dotenv module for loading env vars from .env file

--- database_gui/__main__.py
+++ database_gui/__main__.py
@@ -2,12 +2,16 @@ import psycopg2
 import sys
 from os import getenv
 
+from dotenv import load_dotenv
 from PyQt5.QtWidgets import (
 	QApplication, QWidget, QTabWidget,
 	QAbstractScrollArea, QVBoxLayout, QHBoxLayout,
 	QTableWidget, QGroupBox, QTableWidgetItem,
 	QPushButton, QMessageBox
 )
+
+
+load_dotenv()
 
 
 class MainWindow(QWidget):
