9bba27309b0d74bb134c150caa631100bb8003ae
Danil Sidoruk <danilsidoruknv@yandex.ru>
2022-10-28 21:26:44 +0300

    Initial commit

---
+++ database_gui/__main__.py
@@ -0,0 +1,17 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+
+
+# YOUR CODE HERE
+
+
+def main():
+	app = QApplication(sys.argv)
+
+	# YOUR CODE HERE
+
+	sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+	main()
