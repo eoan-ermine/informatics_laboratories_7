e0ab0f613b36c3f14acb8891485ac303580ffcec
PatriotRossii <patriotrossii2019@mail.ru>
2022-11-08 13:40:24 +0300

    Replace the shit with QDatabaseTableWidget

--- database_gui/__main__.py
+++ database_gui/__main__.py
@@ -9,7 +9,7 @@ from PyQt5.QtWidgets import (
 	QTableWidget, QGroupBox, QTableWidgetItem,
 	QPushButton, QMessageBox
 )
-
+from .database_table_widget import QDatabaseTableWidget
 
 load_dotenv()
 
@@ -21,10 +21,8 @@ class MainWindow(QWidget):
 		self.setWindowTitle("Schedule")
 
 		self.vbox = QVBoxLayout(self)
-		self.tabs = QTabWidget(self)
-		self.vbox.addWidget(self.tabs)
-
-		self._create_schedule_tab()
+		self.vbox.addWidget(QDatabaseTableWidget(self.conn, "bot", "timetable"))
+		self.setLayout(self.vbox)
 
 	def _connect_to_db(self):
 		self.conn = psycopg2.connect(
@@ -33,78 +31,6 @@ class MainWindow(QWidget):
 			host=getenv("PG_HOST"), port=getenv("PG_PORT")
 		)
 		self.cursor = self.conn.cursor()
-
-	def _create_schedule_tab(self):
-		self.schedule_tab = QWidget()
-		self.tabs.addTab(self.schedule_tab, "Schedule")
-
-		self.monday_gbox = QGroupBox("Monday")
-
-		self.svbox = QVBoxLayout()
-		self.shbox1 = QHBoxLayout()
-		self.shbox2 = QHBoxLayout()
-
-		self.svbox.addLayout(self.shbox1)
-		self.svbox.addLayout(self.shbox2)
-
-		self.shbox1.addWidget(self.monday_gbox)
-
-		self._create_monday_table()
-
-		self.update_schedule_button = QPushButton("Update")
-		self.shbox2.addWidget(self.update_schedule_button)
-		self.update_schedule_button.clicked.connect(self._update_schedule)
-
-		self.schedule_tab.setLayout(self.svbox)
-
-	def _create_monday_table(self):
-		self.monday_table = QTableWidget()
-		self.monday_table.setSizeAdjustPolicy(QAbstractScrollArea.AdjustToContents)
-
-		self.monday_table.setColumnCount(4)
-		self.monday_table.setHorizontalHeaderLabels(["Subject", "Time", ", "])
-
-		self._update_monday_table()
-
-		self.mvbox = QVBoxLayout()
-		self.mvbox.addWidget(self.monday_table)
-		self.monday_gbox.setLayout(self.mvbox)
-
-	def _update_monday_table(self):
-		self.cursor.execute("SELECT * FROM bot.timetable WHERE day = 3")
-		records = list(self.cursor.fetchall())
-
-		self.monday_table.setRowCount(len(records) + 1)
-
-		for i, r in enumerate(records):
-			r = list(r)
-			joinButton = QPushButton("join")
-
-			for j in range(3):
-				self.monday_table.setItem(i, j, QTableWidgetItem(str(r[j * 2])))
-			self.monday_table.setCellWidget(i, 3, joinButton)
-
-			joinButton.clicked.connect(lambda ch, num=i: self._change_day_from_table(num))
-
-			self.monday_table.resizeRowsToContents()
-
-	def _change_day_from_table(self, rowNum, day):
-		row = list()
-	
-		for i in range(self.monday_table.columnCount()):
-			try:
-				row.append(self.monday_table.item(rowNum, i).text())
-			except:
-				row.append(None)
-
-		try:
-			self.cursor.execute("TYPE VALID QUERY HERE", tuple(row))
-			self.conn.commit()
-		except:
-			QMessageBox.about(self, "Error", "Enter all fields")
-
-	def _update_schedule(self):
-		self._update_monday_table()
 
 
 def main():
